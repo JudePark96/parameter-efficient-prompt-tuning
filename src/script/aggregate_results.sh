507c2996bb2b93ce16def4b368846f88cc4aca88
JudePark96 <judepark@kookmin.ac.kr>
2022-09-12 18:03:25 +0900

    add: update aggregating results script

--- src/script/aggregate_results.sh
+++ src/script/aggregate_results.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-echo "aggregating prompting and prefix+prompting results"
+echo "aggregating prompting results"
 for base in "roberta-base" "roberta-large"; do
   for length in 10 15 20; do
     for task in "SST-2" "cr" "RTE" "mr"; do
       python3 aggregate_few_shot_results.py "../checkpoints/finetuning/${base}/prompting/pre_seq_len${length}/${task}/" "${task}" "16" "13,21,42,87,100"
-      python3 aggregate_few_shot_results.py "../checkpoints/finetuning/${base}/prefix+prompting/pre_seq_len${length}/${task}/" "${task}" "16" "13,21,42,87,100"
+    done
+  done
+done
+
+echo "aggregating prefix+prompting results"
+for base in "roberta-base" "roberta-large"; do
+  for masking_mode in 'FALSE' 'TRUE'; do
+    for length in 10 15 20; do
+      for task in "SST-2" "cr" "RTE" "mr"; do
+        python3 aggregate_few_shot_results.py "../checkpoints/finetuning/${base}/prefix+prompting/WWM_${masking_mode}/pre_seq_len${length}/${task}/" "${task}" "16" "13,21,42,87,100"
+      done
     done
   done
 done
